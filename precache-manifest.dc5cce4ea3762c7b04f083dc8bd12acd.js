74d943d695d500bd9e0e0f8bf05112e4d47768ec
thabdoul <thabdoul@student.42.fr>
2020-06-23 18:28:33 +0200

    Updates

---
+++ precache-manifest.dc5cce4ea3762c7b04f083dc8bd12acd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b93d0db0b7247782016b14dbc9f476",
+    "url": "/myCV/index.html"
+  },
+  {
+    "revision": "a5389167a5db9987a217",
+    "url": "/myCV/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c42edbe3506a11e17247",
+    "url": "/myCV/static/css/main.765da22e.chunk.css"
+  },
+  {
+    "revision": "a5389167a5db9987a217",
+    "url": "/myCV/static/js/2.64c2ea2f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/myCV/static/js/2.64c2ea2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c42edbe3506a11e17247",
+    "url": "/myCV/static/js/main.a27af1f0.chunk.js"
+  },
+  {
+    "revision": "9033e29e5661d83862b7",
+    "url": "/myCV/static/js/runtime-main.0e1d68b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myCV/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fe83ddf8bd14aac97c0b8d47bd2550a9",
+    "url": "/myCV/static/media/photoprofil.fe83ddf8.png"
+  }
+]);
