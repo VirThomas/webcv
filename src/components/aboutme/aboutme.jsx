9b4385822fdacf8f8c8770756760be2af8e324fa
Thomas.abd <tabdoulazid@vente-privee.com>
2020-09-08 11:25:15 +0200

    :art: Rewrite a propos de moi :D

--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Aboutme = () => {
-    return (
-        <>
-            <h5 className="profileTitle">À propos de moi</h5>
-            <div className="blockContent">
-                <p className="apropos">
-                    Après 3 ans et demi de formation, dont 1 an et demi en entreprise,
-                    je suis en recherche de missions pour pratiquer les technos que j'affectionne particulièrement : Ruby, Rails et React.
-                    </p>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h5 className="profileTitle">À propos de moi</h5>
+      <div className="blockContent">
+        <p className="apropos">
+          Après 3 ans et demi de formation, dont 1 an et demi en entreprise, je
+          suis en recherche de missions pour pratiquer les technos que
+          j'affectionne particulièrement : Ruby/Rails et Node/React.
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Aboutme;
