73eacb9bab6be1f313880f9249b3e1de77c3f771
Thomas ABDOUL-AZID <thomasabd@Air-de-Thomas.numericable.fr>
2024-02-13 13:28:03 +0100

    update cv

--- src/components/proxp/proxp.jsx
+++ src/components/proxp/proxp.jsx
@@ -6,7 +6,8 @@ const Proxp = () => {
         <>
             <h5 className="profileTitle">Expériences Pro</h5>
             <div className="blockContent">
-                <CustomBtn date="11/2020 - Now (2 ans)" company="Plezi" job="Développeur Ruby on Rails" />
+                <CustomBtn date="09/2023 - 02/2024 (6 mois)" company="Freelance" job="Développeur Ruby on Rails" />
+                <CustomBtn date="11/2020 - 06/2023 (2 ans 8 mois)" company="Plezi" job="Développeur Ruby on Rails" />
                 <CustomBtn date="09/2019 - 09/2020 (1 an)" company="Veepee" job="Développeur FullStack" />
                 <CustomBtn date="08/2018 - 02/2019 (6 mois)" company="Captive" job="Développeur Ruby on Rails" />
                 <CustomBtn date="09/2016 - 02/2017 (6 mois)" company="Cora" job="Employé Libre Service" />
