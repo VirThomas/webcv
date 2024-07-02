d0ee1c4be4678e3e4193f8ab726d521c0e45e491
Thomas Abd <thabdoul@student.42.fr>
2024-07-02 17:16:38 +0200

    Update proxp.jsx

--- src/components/proxp/proxp.jsx
+++ src/components/proxp/proxp.jsx
@@ -6,7 +6,7 @@ const Proxp = () => {
         <>
             <h5 className="profileTitle">Expériences Pro</h5>
             <div className="blockContent">
-                <CustomBtn date="09/2023 - 02/2024 (6 mois)" company="Freelance" job="Développeur Ruby on Rails" />
+                <CustomBtn date="09/2023 - Now (10 mois)" company="Freelance" job="Développeur Ruby on Rails" />
                 <CustomBtn date="11/2020 - 06/2023 (2 ans 8 mois)" company="Plezi" job="Développeur Ruby on Rails" />
                 <CustomBtn date="09/2019 - 09/2020 (1 an)" company="Veepee" job="Développeur FullStack" />
                 <CustomBtn date="08/2018 - 02/2019 (6 mois)" company="Captive" job="Développeur Ruby on Rails" />
